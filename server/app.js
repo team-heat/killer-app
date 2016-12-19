7de7041d9f6eccbbbc179f53b48731486dbe06f1
shakuu <todorov.h@gmail.com>
2016-12-19 21:54:32 +0200

    serving with express

--- server/app.js
+++ server/app.js
@@ -1,6 +1,12 @@
 'use strict';
 
 const environment = process.env.NODE_ENV || 'development';
-const config = require('./config/config')[environment];
+const config = require('./config/config')({ environment });
 
-app.listen(config.port);
+const app = require('./config/express.config')({});
+
+require('./config/routes.config')({ app });
+
+app.listen(config.port, function () {
+    console.log('App listening on port: ' + config.port);
+});
--- server/config/express.config.js
+++ server/config/express.config.js
@@ -1,11 +1,15 @@
 'use strict';
 
 const express = require('express');
+const path = require('path');
 
-module.exports = function({}){
+module.exports = function ({}) {
     const app = express();
-    
-    app.use(express.static('../../dist'));
+
+    app.set('view engine', 'pug');
+    app.set('views', path.join(__dirname, '/../../dist'));
+
+    app.use(express.static(path.join(__dirname, '/../../dist/')));
 
     return app;
 };
