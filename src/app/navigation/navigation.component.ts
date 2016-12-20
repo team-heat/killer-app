919ec73aeb9377293925557edaf3991402f6626d
shakuu <todorov.h@gmail.com>
2016-12-20 21:36:22 +0200

    updated styles

--- src/app/navigation/navigation.component.ts
+++ src/app/navigation/navigation.component.ts
@@ -1,21 +1,21 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-    selector: 'app-navigation',
-    templateUrl: './navigation.component.html',
-    styleUrls: ['./navigation.component.scss']
+  selector: 'app-navigation',
+  templateUrl: './navigation.component.html',
+  styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
-    activeItem: string;
+  activeItem: string;
 
-    constructor() { }
+  constructor() { }
 
-    ngOnInit() {
-        this.activeItem = 'Home';
-    }
+  ngOnInit() {
+    this.activeItem = 'Home';
+  }
 
-    setActiveItem(selectedItem: string) {
-        selectedItem = selectedItem || '';
-        this.activeItem = selectedItem;
-    }
+  setActiveItem(selectedItem: string) {
+    selectedItem = selectedItem || '';
+    this.activeItem = selectedItem;
+  }
 }
