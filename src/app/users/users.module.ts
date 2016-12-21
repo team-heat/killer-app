c34550173d3f696e8c42e9efe2ecb1991eb020b6
shakuu <todorov.h@gmail.com>
2016-12-21 20:58:40 +0200

    added scaffolding for users login and register components

--- src/app/users/users.module.ts
+++ src/app/users/users.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UsersComponent } from './users.component';
+import { UsersloginComponent } from './userslogin/userslogin.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [UsersComponent]
+  declarations: [UsersComponent, UsersloginComponent, LoginComponent, RegisterComponent]
 })
 export class UsersModule { }
