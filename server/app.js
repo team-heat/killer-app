de6427fb29ae43832f9d696d9f98c189a03d7cfb
shakuu <todorov.h@gmail.com>
2016-12-23 01:37:52 +0200

    added mongoose

--- server/app.js
+++ server/app.js
@@ -4,13 +4,14 @@ const environment = process.env.NODE_ENV || 'development';
 const config = require('./config/config')({ environment });
 
 const app = require('./config/express.config')({ config });
-
 const userData = require('./data/user-data')({});
 
 require('./config/local.passport.config')({ app, userData });
 require('./config/jwt.passport.config')({ app, userData, config });
 
 require('./config/routes.config')({ app, userData, config });
+
+require('./config/mongoose.config')({ config });
 
 app.listen(config.port, function () {
     console.log(`App listening on port: ${config.port}`);
