319f9aa32d0d710b4a37cb924ac360d680891044
shakuu <todorov.h@gmail.com>
2016-12-23 01:49:24 +0200

    mongoose User

---
+++ server/models/user.model.js
@@ -0,0 +1,32 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  username: String,
+  password: String,
+  firstName: String,
+  lastName: String,
+  email: String
+});
+
+let UserModel;
+
+userSchema.static('createUser', function (user) {
+  return new UserModel({
+    username: user.username,
+    password: user.password,
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email
+  });
+});
+
+userSchema.method('isAuthenticated', function (username, password) {
+  return this.username === username && this.password === password;
+});
+
+mongoose.model('User', userSchema);
+UserModel = mongoose.model('User');
+
+module.exports = function () {
+  return UserModel;
+};
