304d809e3d797b01b7489c1b65d9efa8339fa9a5
shakuu <todorov.h@gmail.com>
2016-12-22 10:11:05 +0200

    added login routes

--- server/config/routes.config.js
+++ server/config/routes.config.js
@@ -1,10 +1,20 @@
 'use strict';
 
 const express = require('express');
+const passport = require('passport');
 const path = require('path');
 
 module.exports = function ({app}) {
     const apiRouter = new express.Router();
+    apiRouter
+        .post('/users', passport.authenticate('local'), function (req, res) {
+            res.status(200).send('POST /api/users');
+        })
+        .put('/users', function (req, res) {
+            // Create user here
+            res.status(200).send('PUT /api/users');
+        });
+
 
     app.use('/api', apiRouter);
 
