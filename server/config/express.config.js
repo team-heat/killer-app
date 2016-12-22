3f7e09d7863db83ca12709a05116734bfda43163
shakuu <todorov.h@gmail.com>
2016-12-22 09:50:22 +0200

    setup express with passport

--- server/config/express.config.js
+++ server/config/express.config.js
@@ -1,11 +1,24 @@
 'use strict';
 
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 const express = require('express');
 const path = require('path');
+const session = require('express-session');
 
-module.exports = function ({}) {
+module.exports = function ({config}) {
     const app = express();
     app.use(express.static(path.join(__dirname, '/../../dist/')));
+
+    app.use(cookieParser());
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.use(session({
+        secret: config.sessionSecret,
+        resave: true,
+        saveUninitialized: true
+    }));
 
     return app;
 };
---
+++ server/config/passport.config.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+
+module.exports = function ({app, userData}) {
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  const localStrategy = new LocalStrategy({
+    username: 'username',
+    password: 'password'
+  }, (username, password, done) => {
+    userData
+      .getUserByUsername(username)
+      .then(user => {
+        if (user) {
+          return {
+            authenticated: user.authenticate(password),
+            user
+          };
+        }
+
+        return done(null, false);
+      })
+      .then((result) => {
+        if (result.authenticated) {
+          return done(null, result.user);
+        }
+
+        return done(null, false);
+      })
+      .catch(err => {
+        return done(err, false);
+      });
+  });
+
+  passport.serializeUser((user, done) => {
+    if (user) {
+      return done(null, user._id);
+    }
+
+    return done(null, null);
+  });
+
+  passport.deserializeUser((id, done) => {
+    userData
+      .getUserById(id)
+      .then(user => {
+        if (!user) {
+          return done(null, false);
+        }
+
+        return done(null, user);
+      })
+      .catch(err => {
+        done(err, false);
+      });
+  });
+
+  passport.use(localStrategy);
+};
