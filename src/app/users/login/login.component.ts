67db807bcd14c7f2a82b39ded2a72c12b77e507d
shakuu <todorov.h@gmail.com>
2016-12-22 13:32:46 +0200

    carousel working

--- src/app/users/login/login.component.ts
+++ src/app/users/login/login.component.ts
@@ -12,10 +12,18 @@ export class LoginComponent implements OnInit {
   user: User;
   isLoading: boolean;
 
+  images: string[];
+
   constructor(userService: UserService) {
     this.userService = userService;
     this.user = new User();
     this.isLoading = false;
+
+    this.images = [
+      'https://www.smashingmagazine.com/wp-content/uploads/2015/06/10-dithering-opt.jpg',
+      'https://c1.staticflickr.com/7/6107/6381966401_032df5fe1e_b.jpg',
+      'https://s3-us-west-1.amazonaws.com/powr/defaults/image-slider2.jpg'
+    ];
   }
 
   ngOnInit() {
