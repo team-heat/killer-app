b2eb880a00bbc1b954915176dead6810c67d0294
shakuu <todorov.h@gmail.com>
2016-12-22 10:14:31 +0200

    fixed indentation

--- server/config/routes.config.js
+++ server/config/routes.config.js
@@ -5,28 +5,29 @@ const passport = require('passport');
 const path = require('path');
 
 module.exports = function ({app}) {
-    const apiRouter = new express.Router();
-    apiRouter
-        .post('/users', passport.authenticate('local'), function (req, res) {
-            res.status(200).send('POST /api/users');
-        })
-        .put('/users', function (req, res) {
-            // Create user here
-            res.status(200).send('PUT /api/users');
-        });
+  const apiRouter = new express.Router();
+  apiRouter
+    .post('/users', passport.authenticate('local'), function (req, res) {
+      // Login user here
+      res.status(200).send('POST /api/users');
+    })
+    .put('/users', function (req, res) {
+      // Create user here
+      res.status(200).send('PUT /api/users');
+    });
 
 
-    app.use('/api', apiRouter);
+  app.use('/api', apiRouter);
 
-    app
-        .get('/', function (req, res) {
-            res
-                .status(200)
-                .sendFile(path.join(__dirname, '/../../dist/index.html'));
-        })
-        .get('*', function (req, res) {
-            res
-                .status(200)
-                .sendFile(path.join(__dirname, '/../../dist/index.html'));
-        });
+  app
+    .get('/', function (req, res) {
+      res
+        .status(200)
+        .sendFile(path.join(__dirname, '/../../dist/index.html'));
+    })
+    .get('*', function (req, res) {
+      res
+        .status(200)
+        .sendFile(path.join(__dirname, '/../../dist/index.html'));
+    });
 };
