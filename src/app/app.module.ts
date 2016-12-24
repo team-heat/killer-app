cee8f075a92b672b5d8bd3ee8f5760616662a795
Iliyan Kupenov <i.kupenov96@gmail.com>
2016-12-25 01:38:04 +0200

    angular modules separated

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,15 @@
-import { AppComponent } from './app.component';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Router } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { CookieService } from 'angular2-cookie/services/cookies.service';
-import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { ToastModule, ToastOptions } from 'ng2-toastr/ng2-toastr';
 import { HomeComponent } from './home/home.component';
 import { HomeModule } from './home/home.module';
-import { HttpModule } from '@angular/http';
 import { NavigationComponent } from './navigation/navigation.component';
-import { NgModule } from '@angular/core';
-import { RouterModule, Router } from '@angular/router';
-import { ToastModule, ToastOptions } from 'ng2-toastr/ng2-toastr';
 import { ToastrNotificationOptionsFactoryService } from './services/toastr-notification-options-factory.service';
 import { ToastrNotificationService } from './services/toastr-notification.service';
 import { ToastrNotificationsHandlerComponent } from './toastr-notifications-handler/toastr-notifications-handler.component';
