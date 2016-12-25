b3e4769a63adbdbe5ce0d7a5d14296631c1f6281
shakuu <todorov.h@gmail.com>
2016-12-25 20:16:43 +0200

    added toasts to logout

--- dist/main.bundle.js
+++ dist/main.bundle.js
@@ -406,10 +406,12 @@ var LoginComponent = (function () {
 /***/ function(module, exports, __webpack_require__) {
 
 "use strict";
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__services_user_service__ = __webpack_require__(155);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_core__ = __webpack_require__(0);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__angular_router__ = __webpack_require__(56);
-/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__services_user_storage_service__ = __webpack_require__(68);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_0__angular_core__ = __webpack_require__(0);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_1__angular_router__ = __webpack_require__(56);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_2__services_toastr_notification_options_factory_service__ = __webpack_require__(221);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_3__services_toastr_notification_service__ = __webpack_require__(153);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_4__services_user_service__ = __webpack_require__(155);
+/* harmony import */ var __WEBPACK_IMPORTED_MODULE_5__services_user_storage_service__ = __webpack_require__(68);
 /* harmony export (binding) */ __webpack_require__.d(exports, "a", function() { return LogoutComponent; });
 var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
     var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
@@ -424,39 +426,49 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 
 
 
+
+
 var LogoutComponent = (function () {
-    function LogoutComponent(appRouter, userService, userStorage) {
+    function LogoutComponent(appRouter, userService, userStorage, toastrNotificationService, toastrNotificationOptionsFactoryService) {
         this.appRouter = appRouter;
         this.userService = userService;
         this.userStorage = userStorage;
+        this.toastrNotificationService = toastrNotificationService;
+        this.toastrNotificationOptionsFactoryService = toastrNotificationOptionsFactoryService;
     }
     LogoutComponent.prototype.ngOnInit = function () {
         var _this = this;
         this.userService.logoutUser()
-            .subscribe(
-        // success
-        function (response) {
+            .subscribe(function (response) {
+            var method = 'success';
+            var message = "Good bye " + _this.userStorage.loggedUser;
+            var heading = 'Aww.';
+            var toastrNotificationOptions = _this.toastrNotificationOptionsFactoryService
+                .createToastrNotificationOptions(method, message, heading);
+            _this.toastrNotificationService.enqueueNotification(toastrNotificationOptions);
             _this.userStorage.clearLoggedUser();
-        }, 
-        // error
-        function (err) {
+        }, function (err) {
             console.log(err);
-        }, 
-        // complete
-        function () {
+            var method = 'error';
+            var message = 'Please try again.';
+            var heading = 'Oops!';
+            var toastrNotificationOptions = _this.toastrNotificationOptionsFactoryService
+                .createToastrNotificationOptions(method, message, heading);
+            _this.toastrNotificationService.enqueueNotification(toastrNotificationOptions);
+        }, function () {
             _this.appRouter.navigateByUrl('/');
         });
     };
     LogoutComponent = __decorate([
-        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_1__angular_core__["Component"])({
+        __webpack_require__.i(__WEBPACK_IMPORTED_MODULE_0__angular_core__["Component"])({
             selector: 'app-logout',
             template: __webpack_require__(710),
             styles: [__webpack_require__(700)]
         }), 
-        __metadata('design:paramtypes', [(typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_2__angular_router__["b" /* Router */] !== 'undefined' && __WEBPACK_IMPORTED_MODULE_2__angular_router__["b" /* Router */]) === 'function' && _a) || Object, (typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_0__services_user_service__["a" /* UserService */] !== 'undefined' && __WEBPACK_IMPORTED_MODULE_0__services_user_service__["a" /* UserService */]) === 'function' && _b) || Object, (typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_3__services_user_storage_service__["a" /* UserStorageService */] !== 'undefined' && __WEBPACK_IMPORTED_MODULE_3__services_user_storage_service__["a" /* UserStorageService */]) === 'function' && _c) || Object])
+        __metadata('design:paramtypes', [(typeof (_a = typeof __WEBPACK_IMPORTED_MODULE_1__angular_router__["b" /* Router */] !== 'undefined' && __WEBPACK_IMPORTED_MODULE_1__angular_router__["b" /* Router */]) === 'function' && _a) || Object, (typeof (_b = typeof __WEBPACK_IMPORTED_MODULE_4__services_user_service__["a" /* UserService */] !== 'undefined' && __WEBPACK_IMPORTED_MODULE_4__services_user_service__["a" /* UserService */]) === 'function' && _b) || Object, (typeof (_c = typeof __WEBPACK_IMPORTED_MODULE_5__services_user_storage_service__["a" /* UserStorageService */] !== 'undefined' && __WEBPACK_IMPORTED_MODULE_5__services_user_storage_service__["a" /* UserStorageService */]) === 'function' && _c) || Object, (typeof (_d = typeof __WEBPACK_IMPORTED_MODULE_3__services_toastr_notification_service__["a" /* ToastrNotificationService */] !== 'undefined' && __WEBPACK_IMPORTED_MODULE_3__services_toastr_notification_service__["a" /* ToastrNotificationService */]) === 'function' && _d) || Object, (typeof (_e = typeof __WEBPACK_IMPORTED_MODULE_2__services_toastr_notification_options_factory_service__["a" /* ToastrNotificationOptionsFactoryService */] !== 'undefined' && __WEBPACK_IMPORTED_MODULE_2__services_toastr_notification_options_factory_service__["a" /* ToastrNotificationOptionsFactoryService */]) === 'function' && _e) || Object])
     ], LogoutComponent);
     return LogoutComponent;
-    var _a, _b, _c;
+    var _a, _b, _c, _d, _e;
 }());
 //# sourceMappingURL=D:/GitHub/killer-app/src/logout.component.js.map
 
--- src/app/users/logout/logout.component.ts
+++ src/app/users/logout/logout.component.ts
@@ -1,6 +1,8 @@
-import { UserService } from './../../services/user.service';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { ToastrNotificationOptionsFactoryService } from './../../services/toastr-notification-options-factory.service';
+import { ToastrNotificationService } from './../../services/toastr-notification.service';
+import { UserService } from './../../services/user.service';
 import { UserStorageService } from './../../services/user-storage.service';
 
 @Component({
@@ -13,21 +15,32 @@ export class LogoutComponent implements OnInit {
   constructor(
     private appRouter: Router,
     private userService: UserService,
-    private userStorage: UserStorageService) { }
+    private userStorage: UserStorageService,
+    private toastrNotificationService: ToastrNotificationService,
+    private toastrNotificationOptionsFactoryService: ToastrNotificationOptionsFactoryService) { }
 
   ngOnInit() {
     this.userService.logoutUser()
-      .subscribe(
-      // success
-      (response) => {
+      .subscribe((response) => {
+        const method = 'success';
+        const message = `Good bye ${this.userStorage.loggedUser}`;
+        const heading = 'Aww.';
+        const toastrNotificationOptions = this.toastrNotificationOptionsFactoryService
+          .createToastrNotificationOptions(method, message, heading);
+
+        this.toastrNotificationService.enqueueNotification(toastrNotificationOptions);
         this.userStorage.clearLoggedUser();
-      },
-      // error
-      (err) => {
+      }, (err) => {
         console.log(err);
-      },
-      // complete
-      () => {
+
+        const method = 'error';
+        const message = 'Please try again.';
+        const heading = 'Oops!';
+        const toastrNotificationOptions = this.toastrNotificationOptionsFactoryService
+          .createToastrNotificationOptions(method, message, heading);
+
+        this.toastrNotificationService.enqueueNotification(toastrNotificationOptions);
+      }, () => {
         this.appRouter.navigateByUrl('/');
       });
   }
