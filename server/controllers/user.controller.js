e614462ca7252c4af9bdc7e3f5d26fe80247702c
shakuu <todorov.h@gmail.com>
2016-12-25 20:04:55 +0200

    added logout route to server

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -6,6 +6,10 @@ module.exports = function ({userData, config}) {
 
   const webTokenSecret = config.webTokenSecret;
 
+  function logout(req, res) {
+    res.status(200).json({ message: 'POST /api/logout' });
+  }
+
   function login(req, res) {
     const webTokenObject = {
       _id: req.user.id,
@@ -50,6 +54,7 @@ module.exports = function ({userData, config}) {
   return {
     login,
     profile,
-    register
+    register,
+    logout
   };
 };
