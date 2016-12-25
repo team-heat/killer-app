726ec50c82bd633e5ef126ded7468044ccca8cb4
shakuu <todorov.h@gmail.com>
2016-12-25 20:56:36 +0200

    added item listing model

---
+++ src/app/models/item-listing.model.ts
@@ -0,0 +1,12 @@
+export interface ItemListing {
+  make: String;
+  model: String;
+  year: String;
+  price: Number;
+  exteriorColor: String;
+  interiorColor: String;
+  engineTorque: Number;
+  enginePower: Number;
+  history: String;
+  createdOn: Date;
+}
