7430f80772c520d6f9e088af105e09a7ed2783a2
ilievv <dmxyyyy@gmail.com>
2016-12-26 22:28:48 +0200

    item listing factory created

---
+++ src/app/services/item-listing-factory.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { ItemListing } from '../models/item-listing.model';
+
+@Injectable()
+export class ItemListingFactoryService {
+
+  constructor() { }
+
+  createItemListing(): ItemListing {
+    
+
+    return null;
+  }
+}
