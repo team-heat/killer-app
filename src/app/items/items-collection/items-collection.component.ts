47b73c3c98e5b8c51548e341b8897be4f7fd6eea
Stoiko Neykov <stoikoneykov@gmail.com>
2016-12-27 07:47:18 +0200

    ItemCollectionComponent now work with the new service

--- src/app/items/items-collection/items-collection.component.ts
+++ src/app/items/items-collection/items-collection.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 
-import { ItemsService } from './../../services/item.service';
+import { ItemListing } from './../../models/item-listing.model';
+
+import { ItemListingService } from './../../services/item-listing.service';
 
 @Component({
     selector: 'items-collection',
@@ -8,33 +10,18 @@ import { ItemsService } from './../../services/item.service';
 })
 
 export class ItemsCollectionComponent implements OnInit {
-    service: ItemsService;
-    items: any;
+    items: ItemListing[];
 
-    constructor(service: ItemsService) {
-        this.service = service;
+    constructor(private service: ItemListingService) {
     }
 
     ngOnInit() {
-        this.items = [];
+        let temp;
 
-        for (let i = 0; i < 5; i += 1) {
-            this.items.push(this.mockedItem);
-        }
+        this.service.getItemsCollection()
+            .map(x => x.json())
+            .subscribe(x => temp = x);
 
-        // this.service.getCollectionOfItems()
-        //     .subscribe(x => this.items = x);
-    }
-
-    private mockedItem = {
-        id: 13,
-        brand: 'Lexus',
-        model: 'LC 500h',
-        year: "2018",
-        pictures: [
-            'http://www.lexus-int.com/models/LC/images/LC-500h.jpg',
-            'https://i.ytimg.com/vi/7v9QpFaQHjE/maxresdefault.jpg'
-        ],
-        price: 100000
+        temp.ForEach(x => this.items.push(x as ItemListing));
     }
 }
