0ba89edef04e6eee5093c327a86578521917e6b0
Stoiko Neykov <stoikoneykov@gmail.com>
2016-12-27 07:15:28 +0200

    recover other items folders

---
+++ src/app/items/add-item/add-item.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms' 
+
+
+@Component({
+    selector: 'add-item',
+    templateUrl: './add-item.component.html'
+})
+
+export class AddItemComponent {
+
+}
