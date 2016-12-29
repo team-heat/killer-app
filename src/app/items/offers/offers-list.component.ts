c1deef97315d26337fa75035e885ec42a3469b60
Stoiko Neykov <stoikoneykov@gmail.com>
2016-12-29 10:27:29 +0200

    added item in offers-component

--- src/app/items/offers/offers-list.component.ts
+++ src/app/items/offers/offers-list.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { Params, ActivatedRoute } from '@angular/router';
+
+import { ItemListing } from './../../models/item-listing.model';
+
+import { ItemListingService } from './../../services/item-listing.service';
+import { UserStorageService } from './../../services/user-storage.service';
 
 @Component({
     selector: 'offers-list',
@@ -6,5 +12,49 @@ import { Component } from '@angular/core';
 })
 
 export class OffersListComponent {
+    item: ItemListing;
+    isLogged: Boolean;
 
+    constructor(
+        private itemListingService: ItemListingService,
+        private userStorageService: UserStorageService,
+        private route: ActivatedRoute) {
+
+        this.item = {
+            _id: null,
+            model: null,
+            make: null,
+            year: null,
+            price: null,
+            pictures: [],
+            enginePower: null,
+            interiorColor: null,
+            exteriorColor: null,
+            createdOn: null,
+            isActive: false,
+            isOwner: false,
+            owner: null
+        };
+    }
+
+    ngOnInit() {
+        let id;
+        let username: String;
+
+        if (this.userStorageService.isLogged()) {
+            this.isLogged = true;
+            username = this.userStorageService.getLoggedUser().username;
+        }
+
+        this.route.params
+            .map((params: Params) => params['id'])
+            .subscribe(x => id = x);
+
+        this.itemListingService.getSingleItem(id)
+            .map(x => x.json())
+            .subscribe(x => {
+                this.item = x as ItemListing;
+                this.item.isOwner = this.item.owner === username;
+            });
+    }
 }
