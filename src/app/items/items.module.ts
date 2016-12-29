ad29f7fd702b27dc146e10cf2e3c0a861665d9b8
Stoiko Neykov <stoikoneykov@gmail.com>
2016-12-29 13:37:23 +0200

    added ToastrNotificationOptionsFactoryService to module

--- src/app/items/items.module.ts
+++ src/app/items/items.module.ts
@@ -14,6 +14,7 @@ import { ItemsRouterModule } from './items.routes';
 
 import { ItemListingService } from './../services/item-listing.service';
 import { UserStorageService } from './../services/user-storage.service';
+import { ToastrNotificationOptionsFactoryService } from '../services/toastr-notification-options-factory.service';
 
 @NgModule({
     imports: [
@@ -31,7 +32,8 @@ import { UserStorageService } from './../services/user-storage.service';
     ],
     providers: [
         ItemListingService,
-        UserStorageService
+        UserStorageService,
+        ToastrNotificationOptionsFactoryService
     ]
 })
 
