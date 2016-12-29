8596854c918a6b6aa4feecfca56ee6bc587fb19a
Stoiko Neykov <stoikoneykov@gmail.com>
2016-12-29 12:41:07 +0200

    added make offer component basic

---
+++ src/app/items/offers/make-offer.component.ts
@@ -0,0 +1,68 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router'
+
+import { CarouselListComponent } from './../../carousel-list/carousel-list.component';
+
+import { ItemListing } from './../../models/item-listing.model';
+
+import { ItemListingService } from './../../services/item-listing.service';
+import { UserStorageService } from './../../services/user-storage.service';
+
+@Component({
+    selector: 'single-item',
+    templateUrl: './make-offer.component.html',
+    styleUrls: ['./make-offer.component.scss']
+
+})
+
+export class MakeOfferComponent implements OnInit {
+    item: ItemListing;
+    isLogged: Boolean;
+    isOwner: Boolean;
+
+    constructor(
+        private itemListingService: ItemListingService,
+        private userStorageService: UserStorageService,
+        private route: ActivatedRoute) {
+
+        this.item = {
+            _id: null,
+            model: null,
+            make: null,
+            year: null,
+            price: null,
+            pictures: [],
+            offers:[],
+            enginePower: null,
+            interiorColor: null,
+            exteriorColor: null,
+            createdOn: null,
+            isActive: false,
+            owner: null
+        };
+    }
+
+    ngOnInit() {
+        let id;
+        let username: String;
+
+        if (this.userStorageService.isLogged()) {
+            this.isLogged = true;
+            username = this.userStorageService.getLoggedUser().username;
+        }
+
+        this.route.params
+            .map((params: Params) => params['id'])
+            .subscribe(x => id = x);
+
+        this.itemListingService.getSingleItem(id)
+            .map(x => x.json())
+            .subscribe(x => {
+                this.item = x as ItemListing;
+                this.isOwner = this.item.owner === username;
+            });
+    }
+}
+
+
+
