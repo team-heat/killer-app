e41a87a430c03a954b273b8401f7496aac94fe6a
Stoiko Neykov <stoikoneykov@gmail.com>
2016-12-29 09:52:14 +0200

    creating route for all offers

--- src/app/items/items.routes.ts
+++ src/app/items/items.routes.ts
@@ -1,11 +1,13 @@
 import { SingleItemComponent } from './single-item/single-item.component';
 import { ItemsCollectionComponent } from './items-collection/items-collection.component'
+import { OffersListComponent } from './offers/offers-list.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 let routes = [
+    { path: 'gallery/:id/offers', component: OffersListComponent },
     { path: 'gallery/:id', component: SingleItemComponent },
-    { path: 'gallery', component: ItemsCollectionComponent }
+    { path: 'gallery', component: ItemsCollectionComponent },
 ]
 
 @NgModule({
