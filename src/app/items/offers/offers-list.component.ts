921fc9d53d608dee536524865eee7c610f15685c
Stoiko Neykov <stoikoneykov@gmail.com>
2016-12-29 09:46:21 +0200

    added offers folder and basic for component

---
+++ src/app/items/offers/offers-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'offers-list',
+    templateUrl: './offers-list.component.html'
+})
+
+export class OffersListComponent {
+
+}
